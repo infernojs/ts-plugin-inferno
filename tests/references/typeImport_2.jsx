f99bd6aadd872a0f8e004846239ee59de1725c8e
Sampo Kivistö <sampo.kivisto@live.fi>
2024-08-31 16:52:09 +0300

    Updated typescript

--- tests/references/typeImport_2.jsx
+++ tests/references/typeImport_2.jsx
@@ -2,10 +2,9 @@
 var $inferno = require("inferno");
 var createVNode = $inferno.createVNode;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Visualizer = void 0;
+exports.Visualizer = Visualizer;
 var test_1 = require("./test");
 function Visualizer(_a) {
     var number = _a.number, other = _a.other;
     return (createVNode(1, "div", "visualizer test", [test_1.a, number, other], 0));
 }
-exports.Visualizer = Visualizer;
