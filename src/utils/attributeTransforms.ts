af92e8866a61223ac5feee120d9382b77315ddd8
Sampo Kivistö <sampo.kivisto@live.fi>
2026-09-25 13:28:31 +0300

    fix SVG attributes based on babel-plugin-inferno

---
+++ src/utils/attributeTransforms.ts
@@ -0,0 +1,8 @@
+// React-style attribute names that map to a different attribute name on elements
+export default {
+    acceptCharset: "accept-charset",
+    transformOrigin: "transform-origin", // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform-origin
+    textAnchor: "text-anchor", // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/text-anchor
+    httpEquiv: "http-equiv",
+    htmlFor: "for"
+};
